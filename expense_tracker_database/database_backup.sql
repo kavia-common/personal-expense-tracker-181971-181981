be2651d01d264888c3af0173f82c6c872f7ef145
Kavia AI <production@kavia.ai>
2025-10-28 12:55:35 +0000

    Auto-commit: session cg8041b3e5 - personal-expense-tracker-181971-181981

--- expense_tracker_database/database_backup.sql
+++ expense_tracker_database/database_backup.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database dump
 --
 
-\restrict wZZO20oy6BbJPD7N7TdqNU6KKtGhJ4A0bCR1xEm9lLVvBXbFARwlnttJqcdgbnX
+\restrict bOPIvZcU2ea6MKuyZhqUt6qTHC8TG5OGqsxTDx6evT9GtRfcACSovef9tAtzJXp
 
 -- Dumped from database version 16.10 (Ubuntu 16.10-0ubuntu0.24.04.1)
 -- Dumped by pg_dump version 16.10 (Ubuntu 16.10-0ubuntu0.24.04.1)
@@ -28,9 +28,9 @@ CREATE DATABASE myapp WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE_PROVIDE
 
 ALTER DATABASE myapp OWNER TO postgres;
 
-\unrestrict wZZO20oy6BbJPD7N7TdqNU6KKtGhJ4A0bCR1xEm9lLVvBXbFARwlnttJqcdgbnX
+\unrestrict bOPIvZcU2ea6MKuyZhqUt6qTHC8TG5OGqsxTDx6evT9GtRfcACSovef9tAtzJXp
 \connect myapp
-\restrict wZZO20oy6BbJPD7N7TdqNU6KKtGhJ4A0bCR1xEm9lLVvBXbFARwlnttJqcdgbnX
+\restrict bOPIvZcU2ea6MKuyZhqUt6qTHC8TG5OGqsxTDx6evT9GtRfcACSovef9tAtzJXp
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -89,5 +89,5 @@ ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA public GRANT ALL ON TABLES 
 -- PostgreSQL database dump complete
 --
 
-\unrestrict wZZO20oy6BbJPD7N7TdqNU6KKtGhJ4A0bCR1xEm9lLVvBXbFARwlnttJqcdgbnX
+\unrestrict bOPIvZcU2ea6MKuyZhqUt6qTHC8TG5OGqsxTDx6evT9GtRfcACSovef9tAtzJXp
 
